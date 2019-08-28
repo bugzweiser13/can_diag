5f8cdf532a1cc5c7863a8d23019e96c6ce3db0f7
bugzweiser <bugzweiser@gmail.com>
2019-08-27 19:38:13 -0700

    jaws db create

--- models/genvehlist.js
+++ models/genvehlist.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
         id: {
             type: DataTypes.INTEGER(11),
             allowNull: false,
-            autoIncrement: true
+            // autoIncrement: true
         },
         model: {
             type: DataTypes.STRING(30),
